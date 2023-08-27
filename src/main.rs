833ca0033e09d70e3b6df2f8097e1c74eb43518f
Dilahara MMS <41099408+chapayev0@users.noreply.github.com>
2023-08-27 06:28:50 -0700

    update model

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,35 @@ extern crate gtk;
 extern crate gio;
 
 use gtk::prelude::*;
-use gtk::Builder;
+use gtk::{Builder, TextView, TextBuffer};
+
+fn perform_object_detection(image_data: Vec<f32>, model_path: &str) -> Result<Vec<f32>, Box<dyn std::error::Error>> {
+    // Initialize TensorFlow
+    let mut tf = tf::Session::new()?;
+
+    // Load a pre-trained object detection model
+    let graph = tf::Graph::new();
+    let graph_def = std::fs::read(model_path)?;
+    graph.import_graph_def(&graph_def, &tf)?;
+
+    // Prepare input data (e.g., an image)
+
+    // Run inference
+    let input_op = graph.operation_by_name_required("input_tensor")?;
+    let output_op = graph.operation_by_name_required("output_tensor")?;
+
+    let mut args = tf::SessionRunArgs::new();
+    args.add_feed(&input_op, 0, &image_data);
+    let output = args.request_fetch(&output_op, 0);
+
+    tf.run(&mut args)?;
+
+    let result: Vec<f32> = args.fetch(output)?;
+
+    // Process the detection results
+
+    Ok(result)
+}
 
 fn main() {
     // Initialize GTK
@@ -12,10 +40,27 @@ fn main() {
     let glade_src = include_str!("ui/ui.glade");
     let builder = Builder::from_string(glade_src);
 
+    let image_data: Vec<f32> = "/image/img1.jpg"
+    let model_path = "path/to/your/model.pb";
+
+    let detection_results = perform_object_detection(image_data, model_path)?;
+
+
     // Get main window object from the Glade UI
     let main_window: gtk::Window = builder
         .object("main_window")
         .expect("Couldn't get main_window");
+
+    // Get the button object from the Glade UI
+    let det_btn: gtk::Button = builder
+        .object("det_img_btn")
+        .expect("Couldn't get my_button");
+
+
+    let details_box: TextView = builder
+        .object("detail_box")
+        .expect("Couldn't get my_textview");
+
 
     // Connect the "destroy" signal to quit the application
     main_window.connect_destroy(|_| {
@@ -25,4 +70,17 @@ fn main() {
     // Show the main window and start the GTK main loop
     main_window.show_all();
     gtk::main();
+}
+
+fn image_load(text_view: &TextView){
+
+    println!("shashika");
+
+    if let Some(buffer) = text_view.buffer() {
+        buffer.set_text("clicked");
+    }
+
+
+
+
 }
